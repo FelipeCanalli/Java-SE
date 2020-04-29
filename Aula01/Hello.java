144a4db6cf3a1f7f42b67ad235dcff41eb4e4e16
Felipe Galvão <canallinho@gmail.com>
2020-04-29 16:25:40 -0300

    Adicionei o script hello.java a Aula01

---
+++ Aula01/Hello.java
@@ -0,0 +1,10 @@
+/** 
+    Hello World - via VsCode
+    @author Felipe Galvão
+*/
+
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello World")
+    }
+}
