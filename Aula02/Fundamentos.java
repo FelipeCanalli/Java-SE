6b3c4af699b14a99359c2e4e1868f3475f595d9f
Felipe Galvão <canallinho@gmail.com>
2020-05-03 23:09:32 -0300

    Adicionei o script Fundamentos.java

---
+++ Aula02/Fundamentos.java
@@ -0,0 +1,20 @@
+package cursojava;
+
+public class Fundamentos {
+
+	public static void main(String[] args) {
+		String nome;
+		int idade = 51;
+		char sexo = 'M';
+		double temperatura = 26.7;
+		boolean arCondicionado = false;
+		nome = "Jose de assis filho";
+		System.out.println("Nome: " +nome);
+		System.out.println("Idade: " +idade);
+		System.out.println("Sexo: " +sexo);
+		System.out.println("Temperatura: " +temperatura);
+		System.out.println("Ar Condicionado: " +arCondicionado);
+
+	}
+
+}
